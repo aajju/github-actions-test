2a4517e65e5e29f5852a076f9766a1622d033d6f
aajju <aajju@naver.com>
2025-02-19 11:31:10 +0900

    secret test

--- apitest-script.py
+++ apitest-script.py
@@ -1,10 +1,14 @@
 import requests
+import os
 
 # API URL (예시: JSON 데이터를 제공하는 API)
 url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 # GET 요청 보내기
 response = requests.get(url)
+
+test_value = os.getenv("TEST_VARIABLES")
+print(f"TEST_VARIABLES: {test_value}")
 
 # 응답 상태 코드 확인 (200이면 성공)
 if response.status_code == 200:
